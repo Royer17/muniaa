3b39ecd17843e020f794b3e87479083a321868a4
Royer17 <royer.jak@gmail.com>
2024-05-20 14:05:53 -0500

    roles

--- resources/views/admin/layouts/footer.blade.php
+++ resources/views/admin/layouts/footer.blade.php
@@ -2,5 +2,5 @@
     <div class="float-right d-none d-sm-block">
       <b></b> 
     </div>
-    <strong>Copyright &copy; 2021 <a href="#"> Municipalidad Distrital de Calana</a>.</strong> Todos los Derechos Reservados.
+    <strong>Copyright &copy; 2021 <a href="#"> Municipalidad Distrital de Alto Alianza</a>.</strong> Todos los Derechos Reservados.
   </footer>
