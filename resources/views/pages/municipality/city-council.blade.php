3bbd2549390e76adbd2bf0c9407781ae911e0da3
Luyz <luis.charres.06@gmail.com>
2024-06-02 16:43:44 -0500

    concejo municipal landing view

--- resources/views/pages/municipality/city-council.blade.php
+++ resources/views/pages/municipality/city-council.blade.php
@@ -6,7 +6,7 @@
     <div
       class="flex items-center px-10 md:px-20 bg-dark-blue w-full h-16 md:h-20 text-white"
     >
-      <h1 class="text-sm md:text-3xl uppercase">Visión y Misión</h1>
+      <h1 class="text-sm md:text-3xl uppercase">Concejo Municipal</h1>
 
       
       
@@ -17,23 +17,50 @@
       >
         <div class="flex-1 bg-white shadow-md rounded-2xl h-fit p-4 md:p-6">
           <div>
-            <img
+<!--             <img
               class="w-full mb-5 max-h-[40rem]"
               src="https://picsum.photos/1200/600"
-            />
+            /> -->
             <div class="flex flex-col gap-4">
-              <div>
-                <h2 class="text-xl font-bold mb-4">Visión</h2>
-                <p class="text-sm md:text-base">
-                    {!! $setting->vision !!}
-                </p>
-              </div>
-              <div>
-                <h2 class="text-xl font-bold mb-4">Misión</h2>
-                <p class="text-sm md:text-base">
-                    {!! $setting->vision !!}
-                </p>
-              </div>
+
+
+              @foreach($city_councils as $city_council)   
+              <table width="800" border="0" class="cardprofile" style="margin-bottom: 10px;">
+                  <tbody class="tcuerpo">
+                      <tr>
+                          <td width="370" height="250">
+                              <img class="sombra" src="{{ $city_council->photo }}" style="border-radius: 5px;">
+
+                          </td>
+                          
+                          <td style="padding-left: 20px;">
+                              <table border="0">
+                                  <tbody>
+                                      <tr>
+                                          <td  class="text-titulo pull-left" ><b style="color:#2e8ece"> {{ $city_council->position }}</b></td>
+                                      </tr>
+                                      <tr>
+                                         <td class="text-titulo pull-left">{{ $city_council->name }}</td>
+                                         
+                                      </tr>
+                                      <tr>
+                                          <td class="text-titulo pull-left">{{ $city_council->email }}</td>
+                                          
+                                      </tr>
+                                  </tbody>
+                              </table>    
+                          </td>
+                      </tr>
+                  </tbody>
+              </table>
+              
+              <hr style="color: #0056b2;" /> <!-- separacion horizontal entre filas -->
+              @endforeach
+            
+
+
+
+
             </div>
           </div>
         </div>
