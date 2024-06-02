e10cacb6396cda3f82291a70b9891a519129a812
Luyz <luis.charres.06@gmail.com>
2024-06-02 16:38:00 -0500

    alcalde view

--- resources/views/pages/municipality/mayor.blade.php
+++ resources/views/pages/municipality/mayor.blade.php
@@ -19,19 +19,18 @@
           <div>
             <img
               class="w-full mb-5 max-h-[40rem]"
-              src="https://picsum.photos/1200/600"
+              src="{{ $setting->photo }}"
             />
             <div class="flex flex-col gap-4">
               <div>
-                <h2 class="text-xl font-bold mb-4">Visión</h2>
+                <h2 class="text-xl font-bold mb-4">Alcalde</h2>
                 <p class="text-sm md:text-base">
-                    {!! $setting->vision !!}
+                    {!! $setting->mayor !!}
                 </p>
               </div>
               <div>
-                <h2 class="text-xl font-bold mb-4">Misión</h2>
                 <p class="text-sm md:text-base">
-                    {!! $setting->vision !!}
+                    {!! $setting->description !!}
                 </p>
               </div>
             </div>
