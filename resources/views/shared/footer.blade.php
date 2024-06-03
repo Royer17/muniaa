4a8060c1793dbe100ed853f47aad0a70a4a5c257
Royer17 <royer.jak@gmail.com>
2024-06-02 20:35:56 -0500

    popup

--- resources/views/shared/footer.blade.php
+++ resources/views/shared/footer.blade.php
@@ -43,9 +43,65 @@
     </p>
   </div>
 </footer>
-
-
-  <script
+<div id="news-popup" class="popup-gallery hidden">
+  <a
+    href="http://farm9.staticflickr.com/8242/8558295633_f34a55c1c6_b.jpg"
+    title="The Cleaner"
+    ><img
+      src="http://farm9.staticflickr.com/8242/8558295633_f34a55c1c6_s.jpg"
+      width="75"
+      height="75"
+  /></a>
+  <a
+    href="http://farm9.staticflickr.com/8382/8558295631_0f56c1284f_b.jpg"
+    title="Winter Dance"
+    ><img
+      src="http://farm9.staticflickr.com/8382/8558295631_0f56c1284f_s.jpg"
+      width="75"
+      height="75"
+  /></a>
+  <a
+    href="http://farm9.staticflickr.com/8225/8558295635_b1c5ce2794_b.jpg"
+    title="The Uninvited Guest"
+    ><img
+      src="http://farm9.staticflickr.com/8225/8558295635_b1c5ce2794_s.jpg"
+      width="75"
+      height="75"
+  /></a>
+  <a
+    href="http://farm9.staticflickr.com/8383/8563475581_df05e9906d_b.jpg"
+    title="Oh no, not again!"
+    ><img
+      src="http://farm9.staticflickr.com/8383/8563475581_df05e9906d_s.jpg"
+      width="75"
+      height="75"
+  /></a>
+  <a
+    href="http://farm9.staticflickr.com/8235/8559402846_8b7f82e05d_b.jpg"
+    title="Swan Lake"
+    ><img
+      src="http://farm9.staticflickr.com/8235/8559402846_8b7f82e05d_s.jpg"
+      width="75"
+      height="75"
+  /></a>
+  <a
+    href="http://farm9.staticflickr.com/8235/8558295467_e89e95e05a_b.jpg"
+    title="The Shake"
+    ><img
+      src="http://farm9.staticflickr.com/8235/8558295467_e89e95e05a_s.jpg"
+      width="75"
+      height="75"
+  /></a>
+  <a
+    href="http://farm9.staticflickr.com/8378/8559402848_9fcd90d20b_b.jpg"
+    title="Who's that, mommy?"
+    ><img
+      src="http://farm9.staticflickr.com/8378/8559402848_9fcd90d20b_s.jpg"
+      width="75"
+      height="75"
+  /></a>
+</div>
+<script
   src="https://code.jquery.com/jquery-3.7.1.slim.min.js"
   integrity="sha256-kmHvs0B+OpCW5GVHUNjv9rOmY0IvSIRcf7zGUDTDQM8="
   crossorigin="anonymous"
@@ -53,10 +109,7 @@
 <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
-
-
-<script src="{{ '/js/slides/homepage.min.js'}}"></script>
-<script src="{{ '/js/slides/vision.min.js'}}"></script>
-<script src="{{ '/js/main.min.js'}}"></script>
+<script src="./js/slides/homepage.min.js"></script>
+<script src="./js/main.min.js"></script>
 </body>
 </html>
