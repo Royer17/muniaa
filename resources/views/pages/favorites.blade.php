03b3a71de435c75276210983083464d8b1bc0192
Royer17 <royer.jak@gmail.com>
2024-06-05 12:04:22 -0500

    arreglando vista de favorites blade

--- resources/views/pages/favorites.blade.php
+++ resources/views/pages/favorites.blade.php
@@ -24,33 +24,54 @@
                         @if(count($files))
                             <div>
                                 <h2 class="text-xl font-bold mb-4">Archivos relacionados</h2>
-                                <div class="overflow-x-auto">
-                                    <table class="min-w-full bg-white border border-gray-200">
-                                        <thead>
-                                            <tr class="bg-gray-100 border-b">
-                                                <th class="text-left p-4">#</th>
-                                                <th class="text-left p-4">Title</th>
-                                                <th class="text-left p-4">Link</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            @foreach($files as $key => $file)
-                                                <tr class="border-b">
-                                                    <td class="p-4">{{ $key + 1 }}</td>
-                                                    <td class="p-4 flex items-center">
-                                                        @if(pathinfo($file['url'], PATHINFO_EXTENSION) === 'pdf')
-                                                            <img src="/img/pdf.svg" alt="PDF icon" class="w-6 h-6 mr-2">
-                                                        @endif
-                                                        {{ $file['title'] }}
-                                                    </td>
-                                                    <td class="p-4">
-                                                        <a href="{{ $file['url'] }}" target="_blank" class="text-blue-500 hover:underline"> <img src="/img/pdf.svg" alt="PDF icon" class="w-6 h-6 mr-2"> </a>
-                                                    </td>
-                                                </tr>
-                                            @endforeach
-                                        </tbody>
-                                    </table>
-                                </div>
+                                
+                                  <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+                                  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+
+                                  <div class="overflow-x-auto">
+                                      <table class="min-w-full bg-white shadow-md rounded-lg overflow-hidden">
+                                          <thead class="bg-gray-800 text-white">
+                                              <tr>
+                                                  <th class="py-3 px-4 text-left">#</th>
+                                                  <th class="py-3 px-4 text-left">Archivo</th>
+                                                  <th class="py-3 px-4 text-left">Titulo</th>
+                                                  
+                                              </tr>
+                                          </thead>
+                                          <tbody class="text-gray-700">
+                                              @foreach($files as $key => $file)
+                                              <tr>
+                                                  <td>
+                                                    {{ $key + 1 }}
+                                                  </td>
+                                                  <td class="border-t">
+                                                      <a href="{{ $file['url'] }}" target="_blank" class="flex items-center py-3 px-4 text-green-600">
+                                                          <i class="mdi mdi-file-pdf-box text-red-500 mr-2"></i>
+                                                          <img src="/img/pdf.png" alt="PDF icon" class="w-6 h-6 mr-2">
+                                                      </a>
+                                                  </td>
+                                                  <td class="border-t py-3 px-4">@if(pathinfo($file['url'], PATHINFO_EXTENSION) === 'pdf')
+                                                    
+                                                @endif
+                                                {{ $file['title'] }}</td>
+                                                  
+                                              </tr>
+
+                                              @endforeach
+                                          </tbody>
+                                        
+                                      </table>
+                                  </div>
+
+
+
+  
+
+
+
+
+                                
                             </div>
                         @endif
                     </div>
